4d9055ff575e65730a6bfbdd25065cae439bc14d
cloudsony999 <cloudsony999@gmail.com>
2022-08-10 16:42:10 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstproject';
+  name = 'amitava';
+  company='CG';
+  uname=new FormControl('');
+
+  display()
+  {
+    alert("Welcome students!!!!")
+  }
+  updateName()
+  {
+    this.uname.setValue("SHAMIK");
+
+  }
+
+  products: any=[
+    {
+      title:"LED",
+      price:"50000",
+      brand:"Sony"
+
+    },
+    {
+      title:"Mobile",
+      price:"30000",
+      brand:"Samsung"
+
+
+    },
+    {
+      title:"Washing Machine",
+      price:"40000",
+      brand:"Bostch"
+
+
+    }
+  ]
 }
